07111864ae3e13888871070daab8b64f69b78a85
Joerg Schultze-Lutter <76180229+joergschultzelutter@users.noreply.github.com>
2021-10-05 17:47:18 +0200

    Initial release

--- src/bnetza-parser.py
+++ src/bnetza-parser.py
@@ -3,6 +3,12 @@
 # Bundesnetzagentur Rufzeichenliste (PDF) Parser
 # Author: Joerg Schultze-Lutter, 2021
 #
+# Herunterladen der jeweils aktuellen Bundesnetzagentur-Rufzeichenliste.
+# Anschließend werden alle Rufzeichen extrahiert und in einem
+# CSV-Format aufbereitet. Der Output erfolgt nach stdout.
+# Das Programm nimmt keine Kategorisierung vor; d.h. auch Klubstationen
+# usw. sind in der Ausgabedatei mit enthalten.
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
@@ -36,82 +42,107 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def get_rufzeichen_file(request_url: str = "https://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Telekommunikation/Unternehmen_Institutionen/Frequenzen/Amateurfunk/Rufzeichenliste/Rufzeichenliste_AFU.pdf?__blob=publicationFile"):
-	
-	request_headers = {"User-Agent": "Mozilla"}
-	file_blob = None
+# Herunterladen der Rufzeichenliste von der Bundesnetzagentur
+def get_rufzeichen_file(
+    request_url: str = "https://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Telekommunikation/Unternehmen_Institutionen/Frequenzen/Amateurfunk/Rufzeichenliste/Rufzeichenliste_AFU.pdf?__blob=publicationFile",
+):
 
-	try:
-		 resp = requests.get(url=request_url,headers=request_headers)
-	except:
-		resp = None
-	
-	if resp:
-		if resp.status_code == 200:
-			file_blob = resp.content
-	else:
-		logger.info(msg="Cannot download Rufzeichen file")
-	
-	return file_blob
+    request_headers = {"User-Agent": "Mozilla"}
+    file_blob = None
+
+    try:
+        resp = requests.get(url=request_url, headers=request_headers)
+    except:
+        resp = None
+
+    if resp:
+        if resp.status_code == 200:
+            file_blob = resp.content
+    else:
+        logger.info(msg="Cannot download Rufzeichen file")
+
+    return file_blob
+
 
 if __name__ == "__main__":
-	print ("Downloading file")
-	file_content= get_rufzeichen_file()
-	if file_content:
+    file_content = get_rufzeichen_file()
+    if file_content:
+        output_string = StringIO()
 
+        # Create the parser
+        parser = PDFParser(BytesIO(file_content))
 
-		output_string = StringIO()
+        # Create the document
+        document = PDFDocument(parser)
 
+        # Check if the file is password protected and abort, if necessary
+        if not document.is_extractable:
+            raise PDFDocument.PDFTextExtractionNotAllowed
 
-		print ("Parsing")
-		# Create the parser
-		parser = PDFParser(BytesIO(file_content))
+        rsrcmgr = PDFResourceManager()
+        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
 
-		print ("Create doc")
-		# Create the document
-		document = PDFDocument(parser)
+        regex_string = r"^(D[A-D|F-R][0-9][A-Z]{1,3}),\s(A|E),"
 
-		# Check if the file is password protected and abort, if necessary
-		if not document.is_extractable:
-			raise PDFDocument.PDFTextExtractionNotAllowed
+        # loop over all pages in the document
+        for page in PDFPage.create_pages(document):
+            interpreter.process_page(page)
 
-		rsrcmgr = PDFResourceManager()
-		device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-		interpreter = PDFPageInterpreter(rsrcmgr, device)
+            # Split up the page by lines
+            lines = output_string.getvalue().split("\n")
 
-		print ("Loop doc")
-		# loop over all pages in the document
-		for page in PDFPage.create_pages(document):
-			interpreter.process_page(page)
-			lines = output_string.getvalue().split("\n") 
+            # Target field which will contain the catenated contents
+            parser_value = ""
 
-			parser_value = ""
-			attach_value = False
+            # Triggers whether values should be attached or not
+            attach_value = False
 
-			for line in lines:
-				matches = re.search(r"^(D[A-D|F-R][0-9][A-Z]{1,3}),\s(A|E),",line)
-				if matches:
-					if parser_value != "":
-						matches = re.search(r"^(D[A-D|F-R][0-9][A-Z]{1,3}),\s(A|E),",parser_value)
-						if matches:
-							print (parser_value)
-					parser_value = line
-				else:
-					if "Liste der" in line:
-						attach_value = False
-						if parser_value != "":
-							matches = re.search(r"^(D[A-D|F-R][0-9][A-Z]{1,3}),\s(A|E),",parser_value)
-							if matches:
-								print (parser_value)
-						parser_value = ""
-					elif "Seite" in line:
-						attach_value = True
-					else:
-						if attach_value and line.strip() != "":
-							parser_value = parser_value + line
-			if parser_value != "":
-				matches = re.search(r"^(D[A-D|F-R][0-9][A-Z]{1,3}),\s(A|E),",parser_value)
-				if matches:
-					print (parser_value)
-			output_string.seek(0)
-			output_string.truncate(0)
+            # loop over all lines on this page
+            for line in lines:
+                # Regex represents potential valid call sign
+                matches = re.search(regex_string, line)
+                if matches:
+                    # Is the value not empty?
+                    if parser_value != "":
+                        # And does its value start with something that looks like a call sign?
+                        matches = re.search(regex_string, parser_value)
+                        if matches:
+                            # transpose the value to a unified csv format
+                            parser_value = parser_value.replace(", ", ";").replace(
+                                "; ", ";"
+                            )
+                            # and print the value
+                            print(parser_value)
+                    # Accept the value and tell the parser that additional lines can be attached
+                    parser_value = line
+                    attach_value = True
+                else:
+                    # crude but simple header parser
+                    if "Liste der" in line:
+                        # don't attach anything from here on up until we encounter a "Seite" text
+                        attach_value = False
+                        # but ensure to dump the last value from the previous page
+                        if parser_value != "":
+                            matches = re.search(regex_string, parser_value)
+                            if matches:
+                                parser_value = parser_value.replace(", ", ";").replace(
+                                    "; ", ";"
+                                )
+                                print(parser_value)
+                        parser_value = ""
+                    # "Seite" --> end of page; we can start attaching again
+                    elif "Seite" in line:
+                        attach_value = True
+                    # default handler; attach the string if we have retrieved something
+                    else:
+                        if attach_value and line.strip() != "":
+                            parser_value = parser_value + line
+            # and dump the last element from the page that we've just processed
+            if parser_value != "":
+                matches = re.search(regex_string, parser_value)
+                if matches:
+                    parser_value = parser_value.replace(", ", ";").replace("; ", ";")
+                    print(parser_value)
+            output_string.seek(0)
+            output_string.truncate(0)
